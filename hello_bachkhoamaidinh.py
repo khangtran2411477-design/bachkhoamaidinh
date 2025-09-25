50d70ec6510be54466c001e776490c838304d5c7
Tên của bạn <Email_dung_dang_ky_GitHub>
2025-09-25 21:35:57 +0700

    Add requirements.txt and hello_bachkhoamaidinh.py

---
+++ hello_bachkhoamaidinh.py
@@ -0,0 +1,19 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from scipy import stats
+
+print("Hello Bach Khoa Mai Dinh! 🎉")
+
+# Ví dụ nhỏ: numpy
+data = np.array([1, 2, 3, 4, 5])
+print("Mean =", np.mean(data))
+
+# DataFrame ví dụ
+df = pd.DataFrame({"x": [1, 2, 3], "y": [4, 5, 6]})
+print(df)
+
+# Vẽ đồ thị nhỏ
+plt.plot([1, 2, 3], [4, 5, 6])
+plt.title("Demo plot - bachkhoamaidinh")
+plt.show()
